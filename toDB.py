486204dc0dbd7e9030e678d538b234d3ac02f73f
madina <madinaa.1467@gmail.com>
2022-09-09 07:15:26 +0600

    Add create indesx, search not finished

--- toDB.py
+++ toDB.py
@@ -25,52 +25,113 @@ from pandas.core.common import SettingWithCopyWarning
 
 warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
+
 def createIndex():
     es = Elasticsearch("http://localhost:9200")
 
-    if es.indices.exists(index="index"):
-        body = "\"mappings\": {" \
-        "\"_doc\": {" \
-               "\"properties\": { \"Drug\": {" \
-                "\"type\": \"text\"," \
-                    "\"fields\": {" \
-                        "\"keyword\": {" \
-                            "\"ignore_above\": 256," \
-                            "\"type\": \"keyword\" " \
-                            "}" \
-                    "}" \
-               "}," \
-               "\"GCNNet_kd\": {" \
-                    "\"type\": \"float\"" \
-               "}," \
-               "\"Target\": {" \
-                    "\"type\": \"text\"," \
-                    "\"fields\": {" \
-                        "\"keyword\": {" \
-                            "\"ignore_above\": 256," \
-                            "\"type\": \"keyword\"" \
-                        "}" \
-                    "}" \
-               "}," \
-               "\"GCNNet_ic50\": {" \
-                    "\"type\": \"float\"" \
-               "}," \
-               "\"GATNet_ic50\": {" \
-               "    \"type\": \"float\"" \
-               "}," \
-               "\"GAT_GCN_ic50\": {" \
-               "    \"type\": \"float\"" \
-               "}," \
-               "\"GCNNet_ki\": {" \
-               "    \"type\": \"float\"" \
-               "}," \
-               "\"GAT_GCN_ki\": {" \
-               "    \"type\": \"float\"" \
-                "}" \
-            "}" \
-        "}" \
-    "}"
-        es.indices.create(index="binding", body=body, params=None, headers=None)
+    # if es.indices.exists(index="index"):
+    #     body = "\"mappings\": {" \
+    #     "\"_doc\": {" \
+    #            "\"properties\": { \"Drug\": {" \
+    #             "\"type\": \"string\"," \
+    #                 "\"fields\": {" \
+    #                     "\"raw\": {" \
+    #                         "\"index\": \"not_analyzed\"," \
+    #                         "\"ignore_above\": 256," \
+    #                         "\"type\": \"string\" " \
+    #                         "}" \
+    #                 "}" \
+    #            "}," \
+    #            "\"GCNNet_kd\": {" \
+    #                 "\"type\": \"float\"" \
+    #            "}," \
+    #            "\"Target\": {" \
+    #                 "\"type\": \"string\"," \
+    #                 "\"fields\": {" \
+    #                     "\"raw\": {" \
+    #                         "\"index\": \"not_analyzed\"," \
+    #                         "\"ignore_above\": 256," \
+    #                         "\"type\": \"string\"" \
+    #                     "}" \
+    #                 "}" \
+    #            "}," \
+    #            "\"GCNNet_ic50\": {" \
+    #                 "\"type\": \"float\"" \
+    #            "}," \
+    #            "\"GATNet_ic50\": {" \
+    #            "    \"type\": \"float\"" \
+    #            "}," \
+    #            "\"GAT_GCN_ic50\": {" \
+    #            "    \"type\": \"float\"" \
+    #            "}," \
+    #            "\"GCNNet_ki\": {" \
+    #            "    \"type\": \"float\"" \
+    #            "}," \
+    #            "\"GAT_GCN_ki\": {" \
+    #            "    \"type\": \"float\"" \
+    #             "}" \
+    #         "}" \
+    #     "}" \
+    # "}"
+
+    # body = '{"settings" : {"number_of_shards": 5,"number_of_replicas": 1},' \
+    #        '"mappings": { "bind": { "_index": {"enabled": true},' \
+    #        '"_id": {"store": "yes"},' \
+    #        '"dynamic_templates": [{"strings": {"match_mapping_type": "string","match": "*",' \
+    #        '"mapping": {"type": "string","index": "not_analyzed"}}}],' \
+    #        '{"properties": {' \
+    #        '"Drug": {"type": "string", "index": "not_analyzed"},' \
+    #        '"Target": {"type": "string", "index": "not_analyzed"},' \
+    #        '"GCNNet_kd": {"type": "float"},' \
+    #        '"GATNet_kd": {"type": "float"},' \
+    #        '"GAT_GCN_kd": {"type": "float"},' \
+    #        '"GCNNet_ic50": {"type": "float"},' \
+    #        '"GATNet_ic50": {"type": "float"},' \
+    #        '"GAT_GCN_ic50": {"type": "float"},' \
+    #        '"GCNNet_ki": {"type": "float"},' \
+    #        '"GATNet_ki": {"type": "float"},' \
+    #        '"GAT_GCN_ki": {"type": "float"},' \
+    #        '"MLTLE": {"type": "float"}}}} }'
+
+    # check = {"settings" : {"number_of_shards": 5,"number_of_replicas": 1},
+    #                         "mappings": { "bind": { "_index": {"enabled": true},"_id": {"store": "yes"},
+    #                         "dynamic_templates": [{"strings": {"match_mapping_type": "string","match": "*","mapping": {"type": "string","index": "not_analyzed"}}}],
+    #                         {"properties": {"Drug": {"type": "string", "index": "not_analyzed"},
+    #                                         "Target": {"type": "string", "index": "not_analyzed"},
+    #                                         "GCNNet_kd": {"type": "float"},"GATNet_kd": {"type": "float"},
+    #                                         "GAT_GCN_kd": {"type": "float"},"GCNNet_ic50": {"type": "float"},
+    #                                         "GATNet_ic50": {"type": "float"},"GAT_GCN_ic50": {"type": "float"},
+    #                                         "GCNNet_ki": {"type": "float"},"GATNet_ki": {"type": "float"},
+    #                                         "GAT_GCN_ki": {"type": "float"},"MLTLE": {"type": "float"}}}} }
+    #
+    che = {"settings": {"number_of_shards": 5, "number_of_replicas": 1},
+           "mappings": {
+               # "dynamic_templates": [
+               #     {
+               #         "strings": {
+               #             "match_mapping_type": "string",
+               #             "match": "*",
+               #             "mapping": {
+               #                 "type": "text",
+               #                 "index": "not_analyzed"
+               #             }
+               #         }
+               #     }
+               # ],
+               "properties": {
+                   "Drug": {"type": "binary"},
+                   "Target": {"type": "text"},
+                   "GCNNet_kd": {"type": "float"}, "GATNet_kd": {"type": "float"},
+                   "GAT_GCN_kd": {"type": "float"}, "GCNNet_ic50": {"type": "float"},
+                   "GATNet_ic50": {"type": "float"}, "GAT_GCN_ic50": {"type": "float"},
+                   "GCNNet_ki": {"type": "float"}, "GATNet_ki": {"type": "float"},
+                   "GAT_GCN_ki": {"type": "float"}, "MLTLE": {"type": "float"}
+               }
+
+           }
+           }
+    es.indices.create(index="binding", body=che)
+
 
 def addToDB(df):
     es = Elasticsearch("http://localhost:9200")
@@ -81,10 +142,11 @@ def addToDB(df):
     actions = []
     for index, row in df.iterrows():
         actions.append({
-            col_names[i].replace("bdtdc_", ""): row[col_names[i]] for i in range(len(col_names)) if col_names[i].lower() != "y"
+            col_names[i].replace("bdtdc_", ""): row[col_names[i]] for i in range(len(col_names)) if
+            col_names[i].lower() != "y"
         })
 
-        if index % 1 == 0: #13 times # 1356755
+        if index % 1 == 0:  # 13 times # 1356755
             try:
                 response = helpers.bulk(es, actions, index='binding')
                 print("\nRESPONSE:", response)
@@ -130,24 +192,25 @@ def appendToDB(df):
     for index, row in df.iterrows():
         query = '{"query":{"bool":' \
                 '{"must":[{"text":{"Drug":"' + row['Drug'] + '"}},' \
-                         '{"text":{"Target":"' + row['Target'] + '"}}]' \
-                '}},"from":0,"size":10,' \
-                '"sort":[],"aggs":{}} '
-        resp = es.search(index="binding", body=query)
-        print("Got %d Hits:" % resp['hits']['total']['value'])
-        results = resp['hits']['hits']
-        for hit in resp['hits']['hits']:
-            print(hit["_source"])
+                                                             '{"text":{"Target":"' + row['Target'] + '"}}]' \
+                                                                                                     '}},"from":0,"size":10,' \
+                                                                                                     '"sort":[],"aggs":{}} '
+        # resp = es.search(index="binding", body=query)
+        # print("Got %d Hits:" % resp['hits']['total']['value'])
+        # results = resp['hits']['hits']
+        # for hit in resp['hits']['hits']:
+        #     print(hit["_source"])
 
-        # actions.append({
-        #     new_col_names[i]: row[col_names[i]] for i in range(len(col_names)) if col_names[i].lower() != "y"
-        # })
-    #
-    # try:
-    #     response = helpers.bulk(es, actions, index='binding')
-    #     print("\nRESPONSE:", response)
-    # except Exception as e:
-    #     print("\nERROR:", e)
+        actions.append({
+            col_names[i].replace("bdtdc_", ""): row[col_names[i]] for i in range(len(col_names)) if
+            col_names[i].lower() != "y" and col_names[i].lower() != "test"
+        })
+
+    try:
+        response = helpers.bulk(es, actions, index='binding')
+        print("\nRESPONSE:", response)
+    except Exception as e:
+        print("\nERROR:", e)
 
 
 def add_results_toDB():
@@ -172,6 +235,36 @@ def append_results_toDB():
         appendToDB(df)
 
 
+def searchDF(df):
+    es = Elasticsearch("http://localhost:9200")
+
+    col_names = list(df.columns)
+    # new_col_names = [column.replace("bdtdc_", "") for column in col_names]
+
+    actions = []
+    for index, row in df.iterrows():
+        query = '{"query":{"bool":' \
+                '{"must":[{"match":{"Drug":"' + row['Drug'] + '"}},' \
+                          '{"match":{"Target":"' + row['Target'] + '"}}]' \
+                 '}},"from":0,"size":10, "sort":[],"aggs":{}} '
+        resp = es.search(index="binding", body=query)
+        print("Got %d Hits:" % resp['hits']['total']['value'])
+        results = resp['hits']['hits']
+        for hit in resp['hits']['hits']:
+            print(hit["_source"])
+
+
+def search():
+    if not os.path.exists(TEST_RESULTS_PATH_TEST):
+        os.makedirs(TEST_RESULTS_PATH_TEST)
+        print("The new directory is created: ", TEST_RESULTS_PATH_TEST)
+
+    parquet_files = glob.glob(TEST_RESULTS_PATH_TEST + '*_res_res_fake_add.parquet')
+    for file in parquet_files:
+        df = get_dataframe(file)
+        searchDF(df)
+
+
 def get_dataframe(file):
     filename = os.path.splitext(os.path.basename(file))[0]
     print('Transform file: ', filename)
@@ -183,7 +276,8 @@ def get_dataframe(file):
 
 
 if __name__ == '__main__':
+    # search()
+    # createIndex()
     append_results_toDB()
     # delete_all()
     # add_results_toDB()
-
